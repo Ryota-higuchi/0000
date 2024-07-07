9a8355bf2544c835e849700086e2041b1ccd31d6
Ryota-higuchi <156732554+Ryota-higuchi@users.noreply.github.com>
2024-07-07 19:40:22 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -35,27 +35,3 @@
 
   play();
 }
-
-// {
-//   function callback(entries, obs) {
-//     entries.forEach(entry => {
-//       if(!entry.isIntersecting) {
-//         return;
-//       }
-
-//       entry.target.classList.add("appear");
-//       obs.unobserver(entries.target);
-//     });
-//   }
-
-//   const options = {
-//     threshold: 0.2,
-//   };
-
-//   const observer = new IntersectionObserver(callback, options);
-//   const targets = document.querySelectorAll(".animate");
-
-//   targets.forEach(target => {
-//     observer.observe(target);
-//   });
-// }
